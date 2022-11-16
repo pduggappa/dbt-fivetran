bca74e30036a420bf409e6b716935f0679585bca
pduggappa <59191953+pduggappa@users.noreply.github.com>
2022-11-16 14:45:33 -0800

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,8 @@
 
 -- Use the `ref` function to select from other models
 
-select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+-- select *
+-- from {{ ref('my_first_dbt_model') }}
+-- where id = 1
+
+select * from {{ source('wirecutter_final', 'dbt_transformed_1') }}
