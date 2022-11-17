f2acfaa8731ff7d83435272a0a34c55eff818098
pduggappa <59191953+pduggappa@users.noreply.github.com>
2022-11-16 21:32:50 -0800

    Update and rename my_second_dbt_model.sql to fb_daily_page_metrics.sql

---
+++ models/example/fb_daily_page_metrics.sql
@@ -0,0 +1,10 @@
+
+-- Use the `ref` function to select from other models
+
+-- select *
+-- from {{ ref('my_first_dbt_model') }}
+-- where id = 1
+
+select page_id,date(date) as metric_date,page_impressions,page_post_engagements,page_video_views,page_fans,page_actions_post_reactions_anger_total,page_actions_post_reactions_haha_total
+,page_actions_post_reactions_like_total,page_actions_post_reactions_love_total,page_actions_post_reactions_sorry_total,page_actions_post_reactions_wow_total,
+ from {{ source('wirecutter_final', 'daily_page_metrics_total') }}
--- models/example/my_second_dbt_model.sql
+++
@@ -1,8 +0,0 @@
-
--- Use the `ref` function to select from other models
-
--- select *
--- from {{ ref('my_first_dbt_model') }}
--- where id = 1
-
-select * from {{ source('wirecutter_final', 'daily_page_metrics_total') }}
