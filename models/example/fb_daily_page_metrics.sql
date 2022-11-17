0ac1a8cd939c064fc4e4f68a6680a05f0bbdf83a
pduggappa <59191953+pduggappa@users.noreply.github.com>
2022-11-16 22:35:27 -0800

    Update fb_daily_page_metrics.sql

--- models/example/fb_daily_page_metrics.sql
+++ models/example/fb_daily_page_metrics.sql
@@ -4,7 +4,8 @@
 -- select *
 -- from {{ ref('my_first_dbt_model') }}
 -- where id = 1
-select * from
+select  mt.page_id, mt.metric_date,page_impressions,page_post_engagements,page_video_views,page_fans,page_actions_post_reactions_anger_total,page_actions_post_reactions_haha_total
+,page_actions_post_reactions_like_total,page_actions_post_reactions_love_total,page_actions_post_reactions_sorry_total,page_actions_post_reactions_wow_total,consumption_type,page_consumptions_by_consumption_type from
 
 (select page_id,date(date) as metric_date,page_impressions,page_post_engagements,page_video_views,page_fans,page_actions_post_reactions_anger_total,page_actions_post_reactions_haha_total
 ,page_actions_post_reactions_like_total,page_actions_post_reactions_love_total,page_actions_post_reactions_sorry_total,page_actions_post_reactions_wow_total,
@@ -12,5 +13,5 @@ select * from
  
  left join
  
-(select page_consumptions_by_consumption_type from  {{ source('wirecutter_final', 'daily_page_metrics_by_consumption_type') }}) ct
-on mt.metric_date = ct.date(date)
+(select date(date) as metric_date,consumption_type,page_consumptions_by_consumption_type from  {{ source('wirecutter_final', 'daily_page_metrics_by_consumption_type') }}) ct
+on mt.metric_date = ct.metric_date
