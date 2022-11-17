a7d5327e24d3608e9c737dad866f94010b83e986
pduggappa <59191953+pduggappa@users.noreply.github.com>
2022-11-17 10:35:53 -0800

    Update my_first_dbt_model.sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,27 +1,15 @@
+select  * from
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
+(select post_id,date(date) as metric_date,post_clicks,post_impressions,post_reactions_anger_total,post_reactions_haha_total,post_reactions_like_total,post_reactions_love_total,post_reactions_sorry_total,post_reactions_wow_total,
+ post_video_views
+ from {{ source('wirecutter_final', 'lifetime_post_metrics_total') }}) lmt
+ 
+ left join
+ 
+(select post_id,url,media_type from  {{ source('wirecutter_final', 'post_attachment_history') }}) pah
+on lmt.post_id = pah.post_id
 
-    Try changing "table" to "view" below
-*/
+left join
 
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+(select id as post_id,share_count from {{ source('wirecutter_final', 'post_history') }}) ph
+on lmt.post_id = ph.post_id
