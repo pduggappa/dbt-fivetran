10169bb1d126f7f4ee9edb81052f79c013bb4493
pduggappa <59191953+pduggappa@users.noreply.github.com>
2022-11-21 10:55:06 -0800

    Update insta_lifetime_page_metrics.sql

--- models/example/insta_lifetime_page_metrics.sql
+++ models/example/insta_lifetime_page_metrics.sql
@@ -1,2 +1,2 @@
-select id as insta_page_id, followers_count,name from {{ source('instagram_business_fivatran', 'user_history') }}
+select id as insta_page_id, date(_fivetran_synced) as latest_date, followers_count,name from {{ source('instagram_business_fivatran', 'user_history') }}
 
