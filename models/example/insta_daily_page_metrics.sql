106a27db432fd7d1a460192246d13f83ccfd5613
pduggappa <59191953+pduggappa@users.noreply.github.com>
2022-11-21 10:50:54 -0800

    Update insta_daily_page_metrics.sql

--- models/example/insta_daily_page_metrics.sql
+++ models/example/insta_daily_page_metrics.sql
@@ -1,1 +1,1 @@
-select id as insta_page_id,impressions,reach,website_clicks from {{ source('instagram_business_fivatran', 'user_insights') }}
+select id as insta_page_id,impressions,reach,website_clicks,date(date) as metric_date from {{ source('instagram_business_fivatran', 'user_insights') }}
